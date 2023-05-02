fbb1f03f950d90bbf04455630a3f2df0e9360276
datdadev <hackermario54@icloud.com>
2023-05-02 23:16:39 +0700

    igdb test

--- src/app/nes/page.js
+++ src/app/nes/page.js
@@ -1,6 +1,6 @@
 "use client"
 import { useEffect, useState } from "react";
-import {decode} from 'html-entities';
+import { decode } from 'html-entities';
 import NavBar from "../components/navBar";
 import {
     VT323,
@@ -21,15 +21,47 @@ const Nes = () => {
     const [gameList, setGameList] = useState([]);
     useEffect(() => {
         async function getGames() {
-            let response = await fetch(`https://zlink.ddns.net/roms/nes`)
-            const backendHtmlString = await response.text()
+            let responseFromHost = await fetch(`https://zlink.ddns.net/roms/nes`)
+            const backendHtmlString = await responseFromHost.text()
             if (!backendHtmlString.includes('<a')) {
                 return []; // or any other default value you want to use
             }
-            const array = [...decode(backendHtmlString).match(/(?<=>)[\w.\':-]+(?=.nes<)/g)];
-            return array;
-        }
+            const gameNames = [...decode(backendHtmlString).match(/(?<=>)[\w.\':-]+(?=.nes<)/g)];
 
+            let responseFromDatabase = await fetch(`https://api.igdb.com/v4/games`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Client-ID': process.env.CLIENT_ID,
+                    'Authorization': `Bearer ${[process.env.ACCESS_TOKEN]}`,
+                },
+                body: `fields name, cover.url;
+where name = "${(gameNames.map(str => str.replaceAll(/-/g, " "))).join('","')}" & cover.url != null;`
+            });
+            let raw_gamesDetail = await responseFromDatabase.json();
+            const gamesDetail = raw_gamesDetail.reduce((accumulator, currentGame) => {
+                // Check if the game's name already exists in the accumulator array
+                const existingGame = accumulator.find(game => game.name === currentGame.name);
+                if (existingGame) {
+                    // If the game already exists, update its cover URL if it's missing
+                    if (!existingGame.cover && currentGame.cover) {
+                        existingGame.cover = currentGame.cover;
+                    }
+                } else {
+                    // If the game doesn't exist yet, add it to the accumulator array
+                    accumulator.push(currentGame);
+                }
+                return accumulator;
+            }, []);
+
+            gamesDetail.map(game => {
+                return {
+                    name: game.name,
+                    cover: game.cover.url
+                };
+            });
+            return gamesDetail
+        }
         getGames().then((array) => setGameList(array))
     }, []);
 
@@ -48,9 +80,8 @@ const Nes = () => {
                         if (game.toLowerCase().includes(value.toLowerCase()))
                             return <GameCard
                                 type="nes"
-                                name={game}
-                                // boxImage={game.imageUrl}
-                                // gamUrl={game.gameUrl}
+                                name={game.name}
+                                cover={game.cover}
                             />
                     })
                 }
