33d9ae87e9c83bec1b58f9f1a2f51a24b1c13a52
datdadev <hackermario54@icloud.com>
2023-05-11 12:49:47 +0700

    fixed scales

--- src/app/components/consoleCard.jsx
+++ src/app/components/consoleCard.jsx
@@ -14,9 +14,9 @@ const vt323 = VT323({
 const ConsoleCard = ({ image, gif, name, numberOfGames }) => {
     return (
         <a href={`/${name.toLowerCase()}`} className='px-4 py-2 group cursor-pointer border-4 border-black hover:border-white flex flex-col items-center'>
-            <div className='relative w-[130px] h-[100px] xl:w-[220px] xl:h-[170px]'>
-                <Image className='absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-100 group-hover:opacity-0 duration-150' height={100} src={image} />
-                <Image className='absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 duration-150' height={100} src={gif} />
+            <div className='relative w-[130px] h-[100px] xl:w-[200px] xl:h-[154px]'>
+                <Image className='w-[130px] h-[100px] xl:w-[200px] xl:h-[154px] absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-100 group-hover:opacity-0 duration-150' src={image} />
+                <Image className='w-[130px] h-[100px] xl:w-[200px] xl:h-[154px] absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 duration-150' src={gif} />
             </div>
             <h2 className={`${vt323.variable} font-vt323 text-2xl xl:text-3xl`}>{name}</h2>
             <p className={`${vt323.variable} font-vt323 -mt-3 text-xl xl:text-2xl text-white/50`}>{numberOfGames} games</p>
